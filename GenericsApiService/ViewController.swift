9e91fe079036f357ecc250047e7fe7b1fff84c1e
Nabin Shrestha <nabin.shrestha@f1soft.com>
2020-05-03 15:53:56 +0545

    Initial Commit

---
+++ GenericsApiService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericsApiService
+//
+//  Created by Nabin Shrestha on 5/3/20.
+//  Copyright © 2020 F1Soft International Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
